ab6b2de633a8364c77465bd5e4a5a19081fc93ef
Alex-Kuroi-Folts <qzer56@gmail.com>
2014-01-17 17:01:35 +0200

    GameScreen updated

--- src/com/me/rvbgame/GameScreen.java
+++ src/com/me/rvbgame/GameScreen.java
@@ -3,12 +3,19 @@ package com.me.rvbgame;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public abstract class GameScreen implements Screen {
 
 	protected final RvbGdxGame mGame;
 	protected final Stage stage;
+	
+    private BitmapFont font;
+    private SpriteBatch batch;
+    private Skin skin;
 	
 	public GameScreen(RvbGdxGame game) {
 		this.mGame = game;
@@ -57,6 +64,33 @@ public abstract class GameScreen implements Screen {
 	@Override
 	public void dispose() {
 		stage.dispose();
+		
+        if( font != null ) font.dispose();
+        if( batch != null ) batch.dispose();
+        if( skin != null ) skin.dispose();
 	}
 
+	public BitmapFont getFont()
+    {
+        if( font == null ) {
+            font = new BitmapFont();
+        }
+        return font;
+    }
+
+    public SpriteBatch getBatch()
+    {
+        if( batch == null ) {
+            batch = new SpriteBatch();
+        }
+        return batch;
+    }
+
+    protected Skin getSkin()
+    {
+        if( skin == null ) {
+        	skin = new Skin( Gdx.files.internal("data/uiskin.json"));
+        }
+        return skin;
+    }
 }
