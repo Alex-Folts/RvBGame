c9be45d8a47fe956eccc78238501c5c6ac086b96
Alex-Kuroi-Folts <qzer56@gmail.com>
2014-02-23 15:37:04 +0200

    update world

--- src/com/me/rvbgame/RvBWorld.java
+++ src/com/me/rvbgame/RvBWorld.java
@@ -9,8 +9,36 @@ public class RvBWorld extends RvBBase {
 	
 	private Image bgImage;
 	
+//	private playerLeft;
+//	private playerRight;
+	
 	public RvBWorld(BattleScreen parentScreen) {
 		super(parentScreen);
 	}
 
+	public static boolean Damage(/*attacker, victim,*/ int attackID) {
+		return false;
+	}
+	
+	public boolean PerformTickDamage() {
+		return false;
+	}
+	
+	public static boolean Heal(/*heller, target,*/ int attackID) {
+		return false;
+	}
+	
+	public boolean CalcTurn() {
+		currentTurnRight = !currentTurnRight;
+		if (currentTurnRight) {
+//			playerRight.beginTurn();
+		} else {
+//			playerLeft.beginTurn();
+		}
+		return true;
+	}
+	
+	public boolean endTurn() {
+		return CalcTurn();
+	}
 }
